1ab11b29524cca3f3e126ef79820cdb9669ca2b8
dmtweedy <damon.tweedy@yahoo.com>
2023-08-01 15:27:02 -0500

    fixed test file, fixed shapes

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function generateLogo(userInput) {
   const svgLogo = `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
       ${shape.render()}
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${logoText}</text>
+      <text x="150" y="125" font-size="40" text-anchor="middle" fill="${textColor}">${logoText}</text>
     </svg>
   `;
 
